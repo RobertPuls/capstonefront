62283c474e14915fb5bcea1e688b3f5818a999ee
Robert Puls <rpu1s@sbcglobal.net>
2017-09-15 11:40:54 -0600

    added stop button

--- app.js
+++ app.js
@@ -1,40 +1,8 @@
-$("#submit").on("click", () => {
-
-  let right = $(".r");
-  let left = $(".l");
-
-
-  for (var i = 0; i < right.length; i++) {
-    right[i] = right[i].checked;
-    left[i] = left[i].checked;
-  }
-
-  let pattern = [
-    [],
-    []
-  ]
-  console.log("right", right);
-
-  // pattern.push(right);
-  // pattern.push(left);
-
-  pattern[0] = right;
-  pattern[1] = left;
-
-  let data1 = {
-    "pattern": pattern
-  }
-  let data2 = JSON.parse(JSON.stringify({
-    "pattern": pattern
-  }))
-
-  console.log(data1.pattern);
-  console.log(data2.pattern);
-
+function makeRequest(url, data) {
   var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "http://10.6.67.80:3000/",
+    "url": url,
     "method": "POST",
     "headers": {
       "content-type": "application/json",
@@ -43,59 +11,55 @@ $("#submit").on("click", () => {
     },
     "processData": false,
     "data": JSON.stringify({
-      "pattern": pattern
+      "pattern": data
     })
   }
 
   $.ajax(settings).done(function(response) {
     console.log(response);
   });
+}
+
+$("#play").on("click", () => {
+
+  let right = $(".r");
+  let left = $(".l");
+  let bass = $(".b");
+  let hihat = $(".hh");
+
+  for (var i = 0; i < right.length; i++) {
+    right[i] = right[i].checked;
+    left[i] = left[i].checked;
+    bass[i] = bass[i].checked;
+    hihat[i] = hihat[i].checked;
+  }
+
+  let pattern = [];
+
+  pattern.push(right);
+  pattern.push(left);
+  pattern.push(bass);
+  pattern.push(hihat);
+
+  console.log(pattern);
+
+  makeRequest("http://10.6.67.80:3000/", pattern);
 });
 
+//right arrow, right snare
 document.addEventListener('keydown', function(e) {
   var key = e.which || e.key;
   console.log(key);
   if (key === 39) {
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "http://10.6.67.80:3000/right",
-      "method": "POST",
-      "headers": {
-        "content-type": "application/json",
-        "cache-control": "no-cache",
-        "postman-token": "5a325911-53eb-6a2c-8838-710eaec7a39f"
-      },
-      "processData": false,
-      "data": "{}\n"
-    }
-
-    $.ajax(settings).done(function(response) {
-      console.log(response);
-    });
+    makeRequest("http://10.6.67.80:3000/right", {});
   }
 });
 
+//left arrow, left snare
 document.addEventListener('keydown', function(e) {
   var key = e.which || e.key;
   console.log(key);
   if (key === 37) {
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "http://10.6.67.80:3000/left",
-      "method": "POST",
-      "headers": {
-        "content-type": "application/json",
-        "cache-control": "no-cache",
-        "postman-token": "5a325911-53eb-6a2c-8838-710eaec7a39f"
-      },
-      "processData": false,
-      "data": "{}\n"
-    }
-
-    $.ajax(settings).done(function(response) {
-      console.log(response);
-    });
+    makeRequest("http://10.6.67.80:3000/right", {});
   }
 });
